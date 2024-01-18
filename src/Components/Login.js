07082f441f46edde8f17e8a1dd80718d1efcf1aa
Rohit Malviya <rohitbmalviya@gmail.com>
2024-01-18 11:52:02 +0530

    Create the react and setup ant design and folder structure

---
+++ src/Components/Login.js
@@ -0,0 +1,12 @@
+import { Grid, Layout } from "antd";
+import React from "react";
+
+function Login() {
+  return (
+    <Layout>
+      <Grid>Hello</Grid>
+    </Layout>
+  );
+}
+
+export default Login;
